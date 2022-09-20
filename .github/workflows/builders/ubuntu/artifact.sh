1eca2a8db00fe2536b71d00e34e9520d29912f39
eq19 <admin@eq19.com>
2022-09-21 04:42:27 +0700

    make && make install

--- .github/workflows/builders/ubuntu/artifact.sh
+++ .github/workflows/builders/ubuntu/artifact.sh
@@ -65,9 +65,3 @@ docker images --all | sort
 
 echo -e "\n$hr\nCURRENTLY RUNNING\n$hr"
 docker ps
-
-echo -e "\n$hr\nINSTALL DEPENDENCIES\n$hr"
-find /full/path -name '.git' -type d -exec bash -c 'git config --global --add safe.directory ${0%/.git}' {} \;
-sudo git config --global --add safe.directory *
-sudo apt-get install libv8-dev
-sudo apt-get install nodejs
