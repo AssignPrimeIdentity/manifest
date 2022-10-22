036d077eaed442067e4f9da3108a178c47fcae74
eq19 <admin@eq19.com>
2022-10-22 21:03:29 +0700

    ruby-version: '3.0.0'

--- docs/docs/assets/_includes/templates/theme/script/init_environment.sh
+++ docs/docs/assets/_includes/templates/theme/script/init_environment.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Update packages database
+echo -e "$hr\nUPDATE PACKAGES\n$hr"
 pacman -Syu --noconfirm
 
 # Installing git package
+echo -e "$hr\nINSTALL GIT\n$hr"
 pacman -S --noconfirm git
 
 # Installing ruby libraries
+echo -e "$hr\nINSTALL RUBY\n$hr"
 pacman -S --noconfirm ruby${RUBY_VERSION} ruby-bundler
 
 # Setting default ruby version
