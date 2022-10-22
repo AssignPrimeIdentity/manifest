5bf84c3d4f8109af0c6da958d4d39b24f101c8b8
eq19 <admin@eq19.com>
2022-10-22 20:34:15 +0700

    ruby3.1.2

--- assets/_includes/templates/theme/script/init_environment.sh
+++ assets/_includes/templates/theme/script/init_environment.sh
@@ -7,10 +7,10 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm git
 
 # Installing ruby libraries
-pacman -S --noconfirm ruby2.7 ruby-bundler
+pacman -S --noconfirm ruby3.1.2 ruby-bundler
 
 # Setting default ruby version
-cp /usr/bin/ruby-2.7 /usr/bin/ruby
+cp /usr/bin/ruby-3.1.2 /usr/bin/ruby
 
 # debug
 ruby -v && bundle version
