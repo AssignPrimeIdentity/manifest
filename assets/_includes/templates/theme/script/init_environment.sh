61fc4c91e6e56d8416c8c7d76084bd6a5c200867
eq19 <admin@eq19.com>
2022-10-24 01:35:26 +0700

    FeedMapping/deploy@v1

--- assets/_includes/templates/theme/script/init_environment.sh
+++ assets/_includes/templates/theme/script/init_environment.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Update packages database
-# echo -e "$hr\nUPDATE PACKAGES\n$hr"
-# apt update
+echo -e "$hr\nUPDATE PACKAGES\n$hr"
+apt install sudo
+usermod -aG sudo root
+sudo apt update
 
 # Setting default ruby version
 echo -e "$hr\nDEFAULT VERSION\n$hr"
-# gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
-# gem update --system
 ruby -v && bundle version
-node -v && npm -v
+
+# This is a temporary workaround
+# See https://github.com/actions/checkout/issues/766
+git config --global --add safe.directory "*"
 
 # Generate a Gemfile.lock
 # $ docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app combos/ruby_node:3_16 bundle install
--- docs/docs/assets/_includes/templates/theme/theme.sh
+++ docs/docs/assets/_includes/templates/theme/theme.sh
@@ -2,13 +2,16 @@
 set -e
 export WORKING_DIR=${PWD}
 export hr=$(printf '=%.0s' {1..83})
-export SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Get script directory
+SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # Initial default value
 TOKEN=${INPUT_TOKEN}
 ACTOR=${INPUT_ACTOR}
 BRANCH=${INPUT_BRANCH}
 REPOSITORY=${INPUT_REPOSITORY}
+OWNER=${GITHUB_REPOSITORY_OWNER}
 PROVIDER=${INPUT_PROVIDER:=github}
 BUNDLER_VER=${INPUT_BUNDLER_VER:=>=0}
 JEKYLL_SRC=${INPUT_JEKYLL_SRC:=./}
@@ -16,24 +19,19 @@ JEKYLL_CFG=${INPUT_JEKYLL_CFG:=./_config.yml}
 JEKYLL_BASEURL=${INPUT_JEKYLL_BASEURL:=}
 PRE_BUILD_COMMANDS=${INPUT_PRE_BUILD_COMMANDS:=}
 
+# https://stackoverflow.com/a/42137273/4058484
+export JEKYLL_CFG=${JEKYLL_CFG}
+if [[ "${OWNER}" != "eq19" ]]; then
+  export JEKYLL_SRC=${JEKYLL_SRC}/docs
+  sed -i -e "s/eq19/${OWNER}/g" ${JEKYLL_SRC}/${JEKYLL_CFG}
+fi
+
 if [[ -z "${TOKEN}" ]]; then
   echo -e "Please set the TOKEN environment variable."
   exit 1
 fi
 
-if [[ "${OWNER}" != "eq19" ]]; then
-  JEKYLL_SRC=${JEKYLL_SRC}/docs
-  sed -i -e "s/eq19/${OWNER}/g" ${JEKYLL_SRC}/${JEKYLL_CFG}
-fi
-
-export JEKYLL_CFG=${JEKYLL_CFG}
-export BUNDLER_VER=${BUNDLER_VER}
-export JEKYLL_GITHUB_TOKEN=${TOKEN}
-export OWNER=${GITHUB_REPOSITORY_OWNER}
-export PAGES_REPO_NWO=${GITHUB_REPOSITORY}}
-export BUNDLE_PATH=${WORKING_DIR}/vendor/bundle
-export SSL_CERT_FILE=$(realpath .github/hook-scripts/cacert.pem)
-
+# Check parameters and assign default values
 if [[ "${PROVIDER}" == "github" ]]; then
   : ${BRANCH:=gh-pages}
   : ${ACTOR:=${GITHUB_ACTOR}}
@@ -47,20 +45,27 @@ if [[ "${PROVIDER}" == "github" ]]; then
 fi
 
 # Initialize environment
+export JEKYLL_GITHUB_TOKEN=${TOKEN}
+export PAGES_REPO_NWO=$GITHUB_REPOSITORY
+export BUNDLE_PATH=${WORKING_DIR}/vendor/bundle
+# export GEM_HOME=/github/home/.gem/ruby/${RUBY_VERSION}
+# export PATH=$PATH:${GEM_HOME}/bin:$HOME/.local/bin
+export SSL_CERT_FILE=$(realpath .github/hook-scripts/cacert.pem)
 ${SCRIPT_DIR}/script/init_environment.sh
-git config --global --add safe.directory "*"
+
+cd ${JEKYLL_SRC}
 
 # Restore modification time (mtime) of git files
+# echo -e "\nRestore modification time of all git files\n"
 ${SCRIPT_DIR}/script/restore_mtime.sh
-
-# BUNDLE INSTALLATION
-echo -e "$hr\nBUNDLE INSTALLATION\n$hr"
+${SCRIPT_DIR}/script/cleanup_bundler.sh
 gem install bundler -v "${BUNDLER_VER}"
+node -v && npm -v
+
+echo -e "$hr\nBUNDLE INSTALLATION\n$hr"
+CLEANUP_BUNDLER_CACHE_DONE=false
 
 # Clean up bundler cache
-CLEANUP_BUNDLER_CACHE_DONE=false
-${SCRIPT_DIR}/script/cleanup_bundler.sh
-
 cleanup_bundler_cache() {
   echo -e "\nCleaning up incompatible bundler cache\n"
   rm -rf ${BUNDLE_PATH}
@@ -69,7 +74,8 @@ cleanup_bundler_cache() {
 }
 
 # If the vendor/bundle folder is cached in a differnt OS (e.g. Ubuntu),
-# it would cause `jekyll build` failed, we should clean up cache firstly.
+# it would cause `jekyll build` failed, we should clean up the uncompatible
+# cache firstly.
 OS_NAME_FILE=${BUNDLE_PATH}/os-name
 os_name=$(cat /etc/os-release | grep '^NAME=')
 os_name=${os_name:6:-1}
@@ -79,15 +85,14 @@ if [[ "$os_name" != "$(cat $OS_NAME_FILE 2>/dev/null)" ]]; then
   echo -e $os_name > $OS_NAME_FILE
 fi
 
+bundle config cache_all true
+bundle config path $BUNDLE_PATH
+bundle install
+
 # Pre-handle Jekyll baseurl
 if [[ -n "${JEKYLL_BASEURL-}" ]]; then
   JEKYLL_BASEURL="--baseurl ${JEKYLL_BASEURL}"
 fi
-
-bundle config cache_all true
-bundle config path ${BUNDLE_PATH}
-gem update --system
-bundle install
 
 # Check and execute pre_build_commands commands
 if [[ ${PRE_BUILD_COMMANDS} ]]; then
@@ -96,7 +101,6 @@ fi
 
 build_jekyll() {
   echo -e "\nJEKYLL INSTALLATION\n"
-  cd ${JEKYLL_SRC}
   pwd 
   JEKYLL_GITHUB_TOKEN=${TOKEN} bundle exec jekyll build --trace --profile \
     ${JEKYLL_BASEURL} \
