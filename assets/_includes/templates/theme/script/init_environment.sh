3f512dad5aa1e406bd2e177b9c85906a8fa2405d
eq19 <admin@eq19.com>
2022-10-23 11:17:49 +0700

    apt install sudo

--- assets/_includes/templates/theme/script/init_environment.sh
+++ assets/_includes/templates/theme/script/init_environment.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Update packages database
-# echo -e "$hr\nUPDATE PACKAGES\n$hr"
-# pacman -Syu --noconfirm
-
-# Installing git package
-# echo -e "$hr\nINSTALL GIT\n$hr"
-# pacman -S --noconfirm git
-
-# Installing ruby libraries
-# echo -e "$hr\nINSTALL RUBY\n$hr"
-# pacman -S --noconfirm ruby${RUBY_VERSION} ruby-bundler
+echo -e "$hr\nUPDATE PACKAGES\n$hr"
+apt install sudo
+usermod -aG sudo root
+sudo apt update
 
 # Setting default ruby version
 echo -e "$hr\nDEFAULT VERSION\n$hr"
