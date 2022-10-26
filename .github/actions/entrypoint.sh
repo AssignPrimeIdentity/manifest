a4f3387cbc809cc29bf5d7c4e851fecd18c3c63b
eq19 <admin@eq19.com>
2022-10-26 17:23:39 +0700

    FROM combos/ruby_node:3_16

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,76 +1,86 @@
 #!/bin/bash
 set -e
+export WORKING_DIR=${PWD}
+export hr=$(printf '=%.0s' {1..83})
 
 # Get script directory
 SCRIPT_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-WORKING_DIR=${PWD}
 
 # Initial default value
-PROVIDER=${INPUT_PROVIDER:=github}
 TOKEN=${INPUT_TOKEN}
 ACTOR=${INPUT_ACTOR}
-REPOSITORY=${INPUT_REPOSITORY}
 BRANCH=${INPUT_BRANCH}
+REPOSITORY=${INPUT_REPOSITORY}
+OWNER=${GITHUB_REPOSITORY_OWNER}
+PROVIDER=${INPUT_PROVIDER:=github}
 BUNDLER_VER=${INPUT_BUNDLER_VER:=>=0}
 JEKYLL_SRC=${INPUT_JEKYLL_SRC:=./}
 JEKYLL_CFG=${INPUT_JEKYLL_CFG:=./_config.yml}
 JEKYLL_BASEURL=${INPUT_JEKYLL_BASEURL:=}
 PRE_BUILD_COMMANDS=${INPUT_PRE_BUILD_COMMANDS:=}
 
-# Set default bundle path and cache
-BUNDLE_PATH=${WORKING_DIR}/vendor/bundle
-
-echo "Starting the Jekyll Deploy Action"
+# https://stackoverflow.com/a/42137273/4058484
+export JEKYLL_CFG=${JEKYLL_CFG}
+if [[ "${OWNER}" != "eq19" ]]; then
+  export JEKYLL_SRC=${JEKYLL_SRC}/docs
+  sed -i -e "s/eq19/${OWNER}/g" ${JEKYLL_SRC}/${JEKYLL_CFG}
+fi
 
 if [[ -z "${TOKEN}" ]]; then
-  echo "Please set the TOKEN environment variable."
+  echo -e "Please set the TOKEN environment variable."
   exit 1
 fi
 
 # Check parameters and assign default values
 if [[ "${PROVIDER}" == "github" ]]; then
+  : ${BRANCH:=gh-pages}
   : ${ACTOR:=${GITHUB_ACTOR}}
   : ${REPOSITORY:=${GITHUB_REPOSITORY}}
-  : ${BRANCH:=gh-pages}
 
   # Check if repository is available
-  if ! echo "${REPOSITORY}" | grep -Eq ".+/.+"; then
-    echo "The repository ${REPOSITORY} doesn't match the pattern <author>/<repos>"
+  if ! echo -e "${REPOSITORY}" | grep -Eq ".+/.+"; then
+    echo -e "The repository ${REPOSITORY} doesn't match the pattern <author>/<repos>"
     exit 1
   fi
+fi
 
-  # Fix Github API metadata warnings
-  export JEKYLL_GITHUB_TOKEN=${TOKEN}
+# Pre-handle Jekyll baseurl
+if [[ -n "${JEKYLL_BASEURL-}" ]]; then
+  JEKYLL_BASEURL="--baseurl ${JEKYLL_BASEURL}"
 fi
 
 # Initialize environment
-echo "Initialize environment"
-${SCRIPT_DIR}/script/init_environment.sh
+export BUNDLER_VER=${BUNDLER_VER}
+export JEKYLL_GITHUB_TOKEN=${TOKEN}
+export PAGES_REPO_NWO=$GITHUB_REPOSITORY
+export BUNDLE_PATH=${WORKING_DIR}/vendor/bundle
+# export GEM_HOME=/github/home/.gem/ruby/${RUBY_VERSION}
+# export PATH=$PATH:${GEM_HOME}/bin:$HOME/.local/bin
+export SSL_CERT_FILE=$(realpath .github/hook-scripts/cacert.pem)
 
 cd ${JEKYLL_SRC}
 
 # Restore modification time (mtime) of git files
-echo "Restore modification time of all git files"
+# echo -e "\nRestore modification time of all git files\n"
 ${SCRIPT_DIR}/script/restore_mtime.sh
+${SCRIPT_DIR}/script/init_environment.sh
 
-# Check and execute pre_build_commands commands
-if [[ ${PRE_BUILD_COMMANDS} ]]; then
-  echo "Executing pre-build commands"
-  eval "${PRE_BUILD_COMMANDS}"
-fi
-
-echo "Initial comptible bundler"
-${SCRIPT_DIR}/script/cleanup_bundler.sh
-gem install bundler -v "${BUNDLER_VER}"
-
+echo -e "$hr\nBUNDLE INSTALLATION\n$hr"
 CLEANUP_BUNDLER_CACHE_DONE=false
 
 # Clean up bundler cache
 cleanup_bundler_cache() {
-  echo "Cleaning up incompatible bundler cache"
+  echo -e "\nCleaning up incompatible bundler cache\n"
+  ${SCRIPT_DIR}/script/cleanup_bundler.sh
+  gem install bundler -v "${BUNDLER_VER}"
+  
   rm -rf ${BUNDLE_PATH}
   mkdir -p ${BUNDLE_PATH}
   CLEANUP_BUNDLER_CACHE_DONE=true
+
+  bundle config cache_all true
+  bundle config path $BUNDLE_PATH
+  bundle install
 }
 
 # If the vendor/bundle folder is cached in a differnt OS (e.g. Ubuntu),
@@ -82,22 +92,18 @@ os_name=${os_name:6:-1}
 
 if [[ "$os_name" != "$(cat $OS_NAME_FILE 2>/dev/null)" ]]; then
   cleanup_bundler_cache
-  echo $os_name > $OS_NAME_FILE
+  echo -e $os_name > $OS_NAME_FILE
 fi
 
-echo "Starting bundle install"
-bundle config cach_all true
-bundle config path $BUNDLE_PATH
-bundle install
-
-# Pre-handle Jekyll baseurl
-if [[ -n "${JEKYLL_BASEURL-}" ]]; then
-  JEKYLL_BASEURL="--baseurl ${JEKYLL_BASEURL}"
+# Check and execute pre_build_commands commands
+if [[ ${PRE_BUILD_COMMANDS} ]]; then
+  eval "${PRE_BUILD_COMMANDS}"
 fi
 
 build_jekyll() {
-  echo "Starting jekyll build"
-  JEKYLL_ENV=production bundle exec jekyll build \
+  echo -e "\nJEKYLL INSTALLATION\n"
+  pwd 
+  JEKYLL_GITHUB_TOKEN=${TOKEN} bundle exec jekyll build --trace --profile \
     ${JEKYLL_BASEURL} \
     -c ${JEKYLL_CFG} \
     -d ${WORKING_DIR}/build
@@ -105,19 +111,17 @@ build_jekyll() {
 
 build_jekyll || {
   $CLEANUP_BUNDLER_CACHE_DONE && exit -1
-  echo "Rebuild all gems and try to build again"
+  echo -e "\nRebuild all gems and try to build again\n"
   cleanup_bundler_cache
-  bundle install
   build_jekyll
 }
 
-cd ${WORKING_DIR}/build
-
 # Check if deploy on the same repository branch
+cd ${WORKING_DIR}/build
 if [[ "${PROVIDER}" == "github" ]]; then
   source "${SCRIPT_DIR}/providers/github.sh"
 else
-  echo "${PROVIDER} is an unsupported provider."
+  echo -e "${PROVIDER} is an unsupported provider."
   exit 1
 fi
 
--- .github/actions/script/init_environment.sh
+++ .github/actions/script/init_environment.sh
@@ -1,20 +1,73 @@
 #!/bin/bash
 
-# Update packages database
-pacman -Syu --noconfirm
+# identity
+echo -e "\n$hr\nWHOAMI\n$hr"
+chown -R $(id -u):$(id -g) $PWD
+whoami
+pwd
+id
 
-# Installing git package
-pacman -S --noconfirm git
-
-# Installing ruby libraries
-pacman -S --noconfirm ruby2.7 ruby-bundler
+# os version
+echo -e "\n$hr\nOS VERSION\n$hr"
+cat /etc/os-release
+uname -r
 
 # Setting default ruby version
-cp /usr/bin/ruby-2.7 /usr/bin/ruby
+echo -e "$hr\nRUBY_NODE VERSION\n$hr"
+ruby -v && bundler version
+node -v && npm -v
 
-# debug
-ruby -v && bundle version
+# file system
+echo -e "\n$hr\nFILE SYSTEM\n$hr"
+df -h
 
+# ls /
+echo -e "\n$hr\nROOT DIR\n$hr"
+echo "/"
+ls -al /
+
+# root
+echo -e "\n$hr\nROOT PROFILES\n$hr"
+echo "/root"
+[[ "$(whoami)" == "root" ]] && ls -alL /root || sudo bash -c 'ls -alL /root'
+
+# root ssh
+echo -e "\n$hr\nSSH FILES\n$hr"
+echo "/root/.ssh"
+[[ "$(whoami)" == "root" ]] && ls -alL /root/.ssh || sudo bash -c 'ls -alL /root/.ssh'
+
+# home
+echo -e "\n$hr\nHOME PROFILES\n$hr"
+echo $HOME
+ls -al $HOME
+# echo $HOME/.local/bin
+
+# git config
 # This is a temporary workaround
 # See https://github.com/actions/checkout/issues/766
 git config --global --add safe.directory "*"
+
+echo -e "\n$hr\nCONFIG FILE\n$hr"
+cat $HOME/.gitconfig
+
+# workspace
+echo -e "\n$hr\nWORKING DIRECTORY\n$hr"
+echo ${WORKING_DIR}
+ls -al ${WORKING_DIR}
+
+# jekyll source
+echo -e "\n$hr\nJEKYLL DIRECTORY\n$hr"
+pwd
+ls -al ${WORKING_DIR}/${JEKYLL_SRC}
+
+# asset files
+echo -e "\n$hr\nASSET FILES\n$hr"
+# https://stackoverflow.com/a/42137273/4058484
+ls -al ${WORKING_DIR}/${JEKYLL_SRC}/docs/assets
+
+# installed packages
+echo -e "\n$hr\nINSTALLED PACKAGES\n$hr"
+dpkg -l
+
+# Generate a Gemfile.lock
+# $ docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app combos/ruby_node:3_16 bundle install
