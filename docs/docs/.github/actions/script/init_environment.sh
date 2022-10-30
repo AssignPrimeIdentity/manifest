e6e50f3b293ffa436d176bc939ea9948d9356a02
eq19 <admin@eq19.com>
2022-10-31 02:47:04 +0700

    /github/home

--- docs/docs/.github/actions/script/init_environment.sh
+++ docs/docs/.github/actions/script/init_environment.sh
@@ -13,6 +13,7 @@ uname -r
 
 # Setting default ruby version
 echo -e "$hr\nRUBY_NODE VERSION\n$hr"
+RUN chmod -R 777 /github/home 
 pip show tensorflow-gpu && pip -V
 
 # https://stackoverflow.com/a/60945404/4058484
