18b50af697f4d23edadccd1d96b31a5d2d907ecb
eq19 <admin@eq19.com>
2022-11-03 00:17:39 +0700

    spacetimeengine/src

--- docs/.github/workflows/builders/docker/artifact.sh
+++ docs/.github/workflows/builders/docker/artifact.sh
@@ -32,15 +32,16 @@ ls -al ${WORKING_DIR}
 
 # jekyll source
 echo -e "\n$hr\nJEKYLL DIRECTORY\n$hr"
+
 cd /primes/numberGenerator
-
-mv _sass ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_sass _sass
-mv _data ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_data _data
 mv assets ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/assets assets
-
+mv _sass ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_sass _sass
 mv _layouts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_layouts _layouts
 mv _plugins ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_plugins _plugins
 mv _includes ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_includes _includes
+
+cd python/spacetimeengine/src
+mv data ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/data _data
 
 cd ${JEKYLL_SRC}/_data/_base
 mv _maps ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_maps _maps
