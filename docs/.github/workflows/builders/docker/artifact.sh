b07ab7f928f9c7967c80957a563107953728ce56
eq19 <admin@eq19.com>
2022-11-03 00:36:09 +0700

    default.git

--- docs/.github/workflows/builders/docker/artifact.sh
+++ docs/.github/workflows/builders/docker/artifact.sh
@@ -33,20 +33,20 @@ ls -al ${WORKING_DIR}
 # jekyll source
 echo -e "\n$hr\nJEKYLL DIRECTORY\n$hr"
 
-cd /primes/numberGenerator
-mv assets ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/assets assets
-mv _sass ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_sass _sass
-mv _layouts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_layouts _layouts
-mv _plugins ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_plugins _plugins
-mv _includes ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_includes _includes
-
-cd python/spacetimeengine/src
+git clone ${DEFAULT_REPO} && cd default
 mv data ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/data _data
 
 cd ${JEKYLL_SRC}/_data/_base
 mv _maps ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_maps _maps
 mv _feeds ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_feeds _feeds
 mv _posts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_posts _posts
+
+cd /primes/numberGenerator
+mv assets ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/assets assets
+mv _sass ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_sass _sass
+mv _layouts ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_layouts _layouts
+mv _plugins ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_plugins _plugins
+mv _includes ${JEKYLL_SRC}/ && ln -s ${JEKYLL_SRC}/_includes _includes
 
 cd ${JEKYLL_SRC} && pwd
 ls -al .
